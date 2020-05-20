9aaf436de5d71090c54edf124cdf8a416236d013
MrBehavior <cguyton87@gmail.com>
2020-05-20 16:51:07 -0700

    newest commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function closeNav() {
+    document.getElementById("musicSideNav").style.width = "0";
+}
+
+function openNav() {
+    document.getElementById("musicSideNav").style.width = "300px";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    console.log("it works");
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("resSlides");
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
